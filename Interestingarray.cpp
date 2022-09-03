f31d2eafa5ce3d7d1b91ae3102719a99e087a422
surajhe <86972874+surajhe@users.noreply.github.com>
2022-09-03 15:46:00 +0530

    Create Interestingarray.cpp

---
+++ Interestingarray.cpp
@@ -0,0 +1,27 @@
+You have an array A with N elements. We have two types of operation available on this array :
+We can split an element B into two elements, C and D, such that B = C + D.
+We can merge two elements, P and Q, to one element, R, such that R = P ^ Q i.e., XOR of P and Q.
+You have to determine whether it is possible to convert array A to size 1, containing a single element equal to 0 after several splits and/or merge?
+  
+string Solution::solve(vector<int> &A) {
+
+    int ans = 0;
+    int n = A.size();
+
+    for(int i=0; i<n; i++)
+    {
+        ans ^= A[i];
+    }
+
+    if(ans%2 == 0)
+    {
+        return "Yes";
+    }
+    else
+    {
+        return "No";
+    }
+}
+  
+  
+  
